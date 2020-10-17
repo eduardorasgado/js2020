be84520478f784ad715c0ec2f315cb2d9516e8ef
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-10-17 16:47:56 -0500

    Async await

--- node-from-zero-to-hero/fundamentals/02-fundamentos/async-await.js
+++ node-from-zero-to-hero/fundamentals/02-fundamentos/async-await.js
@@ -3,14 +3,24 @@
  * Asyn Await
  */
 
- /**
-  * Async await se invoca con la palabra clave async, esto hace que la funcion 
-  * devuelva una promise
-  */
- let getNombre = async (nombre) => {
-    if(!nombre) throw new Error("No existe el nombre");
-    return nombre.toUpperCase();
- }
+/**
+ * Async await se invoca con la palabra clave async, esto hace que la funcion 
+ * devuelva una promise
+*/
+//let getNombre =  async (nombre) => {
+//   if(!nombre) throw new Error("No existe el nombre");
+//   return nombre.toUpperCase();
+//}
+
+let getNombre = (nombre) => {
+   return new Promise( (resolve, reject) => {
+       if(!nombre) {
+           reject("No existe el nombre");
+       } else {
+           resolve(nombre.toUpperCase());
+       }
+   } );
+};
 
 let nombres = ['Eduardo', 'Pedro', 'Mariano', null];
 
@@ -23,4 +33,30 @@ let nombres = ['Eduardo', 'Pedro', 'Mariano', null];
     .catch( e => {
         console.log(`Error de ASYNC: ${e}`);
     });
+ });
+
+
+ // Trabajando con el await
+ let saludo = async (nombre) => {
+
+     let nombreDB = await getNombre(nombre);
+     return `Hola mundo ${nombreDB}`;
+ };
+
+ saludo("Jerry")
+ .then((s) => {
+     console.log(s);
+ })
+ .catch( e => {
+     console.log("Error de conexion a la DB", e);
+ });
+
+
+ // testeando la funcion de tipo async await
+ saludo(null)
+ .then((s) => {
+     console.log(s);
+ })
+ .catch( e => {
+     console.log("Error de conexion a la DB", e);
  });
