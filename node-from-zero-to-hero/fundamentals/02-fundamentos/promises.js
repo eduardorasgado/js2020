0c86b66aa8ebed5a4f1c4bf23b437798bfc457f8
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-10-17 01:35:27 -0500

    Trabajando con las promesas

--- node-from-zero-to-hero/fundamentals/02-fundamentos/promises.js
+++ node-from-zero-to-hero/fundamentals/02-fundamentos/promises.js
@@ -1,4 +1,34 @@
-// Promesas
 const { empleados, salarios } = require("./DB");
 
-let perro = 1;
+// Promesas
+let getEmpleado = (id) => {
+    // se manda a llamar resolve si hay exito, de otro modo se llama a reject
+    return new Promise( (resolve, reject) => {
+        let empleadoDB = empleados.find((empleado) => {
+            return empleado.id === id
+        });
+    
+        if (!empleadoDB) {
+            return reject(`No existe el empleado con id ${id}`);
+        }
+        // resolve solo recibe un arumento
+        return resolve(empleadoDB);
+    });
+};
+
+// probando la promesa dentro de la arrow function previa
+
+let empleadoIdList = [ 1, 13, 2, 6, 3, 4 ];
+
+// Notese que el resultado de la evaluacion de la promesa en el ciclo for
+// devuelve en cierto orden los resultados de las busquedas, dada la caracteristica
+// Non blocking de node.
+empleadoIdList.forEach( empleadoId => {
+    getEmpleado(empleadoId)
+        .then( empleado => {
+            console.log(`Empleado de BD: ${empleado.nombre}`);
+        })
+        .catch( err => {
+            console.log(err);
+        });
+});
