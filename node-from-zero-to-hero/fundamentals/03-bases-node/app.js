a3fc296a74cbf3b2e4b399b31a9da92ea418385b
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-10-18 21:31:17 -0500

    Separando la funcion para crear archivos.

--- node-from-zero-to-hero/fundamentals/03-bases-node/app.js
+++ node-from-zero-to-hero/fundamentals/03-bases-node/app.js
@@ -1,19 +1,9 @@
 const { time, log } = require("console");
-const fs = require("fs");
+const { crearArchivo } = require("./multiplicar/multiplicar");
 
+let base = 0;
 
-let base = 9;
-let data = '';
-
-for(let i = 1; i <= 10; i++) {
-    data += `${base} * ${i} = ${base * i}\n`;
-};
-
-let location = `tablas/`;
-let fileName = `tabla-${base}.txt`;
-
-// guardando la tabla de base en el fichero del sistema
-fs.writeFile(location+fileName, data, (err) => {
-    if(err) throw new Error("No se pudo crear el archivo.");
-    console.log(`El archivo ${fileName} ha sido creado y guardado`);
-});
+crearArchivo(base)
+    .then( archivo => {
+        console.log(`Se ha creado el archivo de nombre: ${archivo}`)
+    });
---
+++ node-from-zero-to-hero/fundamentals/03-bases-node/multiplicar/multiplicar.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+const crearArchivo = async ( base ) => {
+    let data = '';
+
+    for(let i = 1; i <= 10; i++) {
+        data += `${base} * ${i} = ${base * i}\n`;
+    };
+
+
+    let location = `tablas/`;
+    let fileName = `tabla-${base}.txt`;
+
+    // guardando la tabla de base en el fichero del sistema
+    fs.writeFile(location+fileName, data, (err) => {
+        if(err) throw new Error("No se pudo crear el archivo.");
+        console.log(`El archivo ${fileName} ha sido creado y guardado`);
+    });
+}
+
+exports.crearArchivo = crearArchivo;
