0687fb791f1f9d13934d8d99217b449ce031fe61
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-10-18 21:10:48 -0500

    guardando archivos en el sistema de ficheros de la pc

--- node-from-zero-to-hero/fundamentals/03-bases-node/app.js
+++ node-from-zero-to-hero/fundamentals/03-bases-node/app.js
@@ -1,5 +1,19 @@
-let base = 3;
+const { time, log } = require("console");
+const fs = require("fs");
+
+
+let base = 9;
+let data = '';
 
 for(let i = 1; i <= 10; i++) {
-    console.log(`${base} * ${i} = ${base * i}`);
-}
+    data += `${base} * ${i} = ${base * i}\n`;
+};
+
+let location = `tablas/`;
+let fileName = `tabla-${base}.txt`;
+
+// guardando la tabla de base en el fichero del sistema
+fs.writeFile(location+fileName, data, (err) => {
+    if(err) throw new Error("No se pudo crear el archivo.");
+    console.log(`El archivo ${fileName} ha sido creado y guardado`);
+});
