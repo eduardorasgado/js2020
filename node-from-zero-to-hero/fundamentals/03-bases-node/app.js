bc5c747cdbb9d13891be29458f8fc70f3c90bc18
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-10-19 22:44:30 -0500

    Las opciones de yargs y variables de config de yargs son separados.

--- node-from-zero-to-hero/fundamentals/03-bases-node/app.js
+++ node-from-zero-to-hero/fundamentals/03-bases-node/app.js
@@ -1,33 +1,5 @@
-const argvModule = require('yargs');
 const { crearArchivo, listarTabla } = require("./multiplicar/multiplicar");
-
-
-const baseObj = {
-    demand: true,
-    describe: "La base de la tabla que deseas obtener e.g: --base=9",
-    alias:'b',
-    type: 'number'
-};
-
-const limiteObj = {
-    demand: false,
-    describe: "Limite superior de la tabla requerida, e.g: --limite=5",
-    alias: 'l',
-    default: 10,
-    type: 'number'
-};
-
-const requirements = {
-    base: baseObj,
-    limite: limiteObj
-};
-// para conseguir los argumentos de manera ordenada con ayuda de yargs
-// Configuando los comandos de ayuda para la consola
-argvModule
-    .command('listar', 'Imprime en consola la tabla de multiplicar', 
-    requirements)
-    .command('crear', 'Guarda la tabla de multiplicar en un archivo permanente', 
-    requirements);
+const { argvModule } = require('./config/yargs');
 
 let argv = argvModule.help().argv;
 
