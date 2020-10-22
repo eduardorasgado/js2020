183ec0ef511a9adf8beaac4016fb45baf25b32f1
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-10-22 12:59:25 -0500

    listar, crear, cargarDB, guardarDB creados.

--- node-from-zero-to-hero/fundamentals/04-to-do-app/api/api.js
+++ node-from-zero-to-hero/fundamentals/04-to-do-app/api/api.js
@@ -2,25 +2,30 @@ const fs = require('fs');
 
 let tareasPorHacer = [];
 
-const arranque = () => {
-    fs.readFile('db/data.json', (err, data) => {
-        if(err) return;
-        tareasPorHacer = JSON.parse(data);
+const guardarDB = () => {
+    
+    let data = JSON.stringify(tareasPorHacer);
+    fs.writeFile('db/data.json', data, (err) => {
+        if(err) throw new Error("No se pudo grabar", err);
     });
 }
 
-const guardarDB = async () => {
-    let data = JSON.stringify(tareasPorHacer);
-    fs.writeFile('db/data.json', data, (err) => {
-        if(err) throw new Error('No se pudo guardar la tarea en la DB: '+err.message);
-        else return true;
-    })
+// TODO: Cuando no hay nada en data, presenta un error;
+const cargarDB = () => {
+    tareasPorHacer = require('../db/data.json');
+    
 }
 
-const crear = async (descripcion) => {
+const crear = (descripcion) => {
+
+    cargarDB();
 
     if(!descripcion || descripcion == '') {
-        throw new Error('No existe una descripción.')
+        return {
+            error: true,
+            message: 'No existe una descripción.',
+            data: null
+        };
     }
 
     let porHacer = {
@@ -29,22 +34,40 @@ const crear = async (descripcion) => {
         descripcion,
     };
     tareasPorHacer.push(porHacer);
-    let dbWrite = await guardarDB();
-    
-    return 'Se ha agregado una nueva tarea';
+
+    guardarDB();
+    return {
+        error: false,
+        message: 'La tarea se ha guardado con exito',
+        data: porHacer
+    };
 };
 
 const listar = () => {
-    let tareas = 'Numero Estado    Tarea';
-    if(tareasPorHacer.length == 0) return 'No hay tareas por hacer';
-    tareasPorHacer.forEach(tarea => {
-        tareas += `${tarea.id} ${tarea.completado} -> ${tarea.descripcion} `;
-    });
-    return tareas;
+    cargarDB();
+    
+    let tareas = 'Numero Estado    Tarea\n';
+
+    if(tareasPorHacer.length == 0) {
+        return {
+            error: true,
+            message: '[La Lista está vacía]',
+            data: null
+        };
+    } else {
+        tareasPorHacer.forEach(tarea => {
+            tareas += `${tarea.id}       ${tarea.completado} -> ${tarea.descripcion} \n`;
+        });
+
+        return {
+            error: false,
+            message: 'Todas las tareas han sido recopiladas con exito.',
+            data: tareas
+        }
+    }
 }
 
 module.exports = {
-    arranque,
     crear,
     listar
 }
--- node-from-zero-to-hero/fundamentals/04-to-do-app/app.js
+++ node-from-zero-to-hero/fundamentals/04-to-do-app/app.js
@@ -1,5 +1,6 @@
-const { argv } = require('./config/yargs');
 const api = require('./api/api');
+const { argv } = require('./config/yargs');
+
 
 if(argv.argv._[0] == undefined) {
     console.log('[Introduzca un comando]');
@@ -12,25 +13,25 @@ let command = argv.argv._[0];
 // node app crear -d "Pasear a Solovino"
 // node app listar
 // node app actualizar -d "Pasear a Solovino" -c true
-
-// Llenando la lista de las actividades persistentes
-api.arranque();
-
-const handlingCrear = async(d) => {
-    let result = await api.crear(d)
-    return result;
-};
-
+let response;
 switch(command) {
     case "crear":
-        handlingCrear(argv.argv.descripcion)
-        .then(result => {
-            console.log(result);
-        })
-        .catch(e => {console.log('ERROR: ', e);});
+        response = api.crear(argv.argv.descripcion);
+        if(response.error) {
+            console.error(response.message);
+        } else {
+            console.info(response.message)
+        }
+        
         break;
     case "listar":
-        console.log("Listando todas las tareas registradas hasta el momento");
+        console.log('[TODAS LAS TAREAS]');
+        response = api.listar();
+        if(response.error) {
+            console.error(response.message);
+        } else {
+            console.info(response.data)
+        }
         break;
     case "actualizar":
         console.log("Actualizando una tarea como 'hecha'");
